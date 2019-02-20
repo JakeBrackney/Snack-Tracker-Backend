8443bbe66a46166de0634954759333ed341d3eb3
“MARIA <MDFORTINO@GMAIL.COM>
2019-02-20 17:35:14 -0500

    fix typo in passport.js

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,7 @@ const Strategy = passportJWT.Strategy
 
 const config = require('./config')
 
-const mongoose = require('../models/User')
+const mongoose = require('../db/UserModel') 
 const User = mongoose.model('User')
 
 const params = {
