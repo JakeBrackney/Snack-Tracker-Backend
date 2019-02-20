39bd005c695d7e391be96a075f5dba7d31b98b16
“MARIA <MDFORTINO@GMAIL.COM>
2019-02-20 16:47:48 -0500

    install passport and jwt dependencies, build out initial user auth structure

---
+++ db/UserModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("../db/connection")
+const Schema = mongoose.Schema; 
+const bcrypt = require("bcrypt-nodejs")
+
+const User = new Schema({
+    email: String,
+    password: String,
+    restaurants: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Restaurant"
+      }
+    ]
+  });
+  
+  User.methods.encrypt = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+  };
+  
+  User.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+  
+  
+  module.exports = mongoose.model("User", User);
+  
--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const parser = require("body-parser");
 const cors = require("cors");
 const Restaurant = require('./db/models') 
-
+const passport = require('./config/passport')()
 const app = express();
 
 app.set("port", process.env.PORT || 3001);
+
 app.use(parser.json());
 app.use(cors());
+app.use(passport.initialize())
 
 app.listen(app.get('port'), () => {
     console.log(`✅ PORT: ${app.get('port')} 🌟`)
